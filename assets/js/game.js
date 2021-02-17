43800e6bb24bd357d84969f68edb0a3ae266d4ce
Tela Caul <telacaul@gmail.com>
2021-02-16 16:30:36 -0800

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+var playerName = window.prompt("What is your robot's name?");
+
+//this create4s a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
